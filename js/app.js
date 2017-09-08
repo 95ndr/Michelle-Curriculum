16484d63ce1ee1d27d4f1164959904924bddc084
Michelle <an.darleen@outlook.com>
2017-09-08 16:45:37 -0600

    arreglando width y height canvas

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
 $(document).ready(function(){
+// canvas
 
+ var $c = $("#cuadros");
 
- // canvas
-var dibujo= document.getElementById('cuadros');
-var lienzo = dibujo.getContext("2d");
-console.log(lienzo);
+ $("#cuadros").width(window.innerWidth);
+ var $w = $($c).width();
+ $($c).attr('width', $w);
+
+ $("#cuadros").height(window.innerHeight);
+ var $h = $($c).height();
+ $($c).attr('height', $h);
+
 
 function dibujarY(){
-	var $c = $("#cuadros");
+	
 	var i = 5;
-	while(i < $c.width()){
+	while(i < $c.width() ){
 	$('canvas').drawLine({
   strokeStyle: 'white',
   strokeWidth: 1,
   x1: i, y1: 0,
-  x2: i, y2:699
+  x2: i, y2: $c.height()
 });
-	i+=10
+	i+=5
 	}
 }
+
 dibujarY();
+//responsive 
+
+
+
 }); //fin de toda la app
 
 
