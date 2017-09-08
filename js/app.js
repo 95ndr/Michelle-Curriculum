9ba71bcd8efd39e4b6ddb9556aa6d40cb37b1036
Michelle <an.darleen@outlook.com>
2017-09-07 18:05:49 -0600

    cambio de diseño, animaciones, y contenido

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
+
 $('html').ready(function(){
-	 $("#titulo").slideDown(2000, function() {
- 	$(".navegacion").slideDown(1000);
+	 $(".f").slideUp(5000);
  });
+    
+    
 
-})	
-
- $(".fondo").hover(function() {
-  $("#titulo").slideToggle('slow');
-
-});
 
  // canvas
 
@@ -17,40 +13,41 @@ $('html').ready(function(){
 var dibujo= document.getElementById('cuadros');
 var lienzo = dibujo.getContext("2d");
 console.log(lienzo);
+
 function dibujarY(){
 	var $c = $("#cuadros");
 	var i = 5;
 	while(i < $c.width()){
 	$('canvas').drawLine({
-  strokeStyle: '#000',
+  strokeStyle: 'white',
   strokeWidth: 1,
   x1: i, y1: 0,
   x2: i, y2:799
 });
-	i+=5
+	i+=10
 	}
 }
 dibujarY();
 
-function dibujarX(){
-	  	var $c = $("#cuadros");
+/*	  	var $c = $("#cuadros");
 	var i = 5;
 	while(i < $c.height()){
 	$('canvas').drawLine({
-  strokeStyle: '#000',
+  strokeStyle: 'white',
   strokeWidth: 1,
   x1:0, y1: i,
   x2:1399, y2:i
 });
-	i+=5
+	i+=3
 	}
 }
-dibujarX();
+dibujarX();*/
 
 
 
+var $h = $("html");
 
-});//fin de toda la app
+}); //fin de toda la app
 
 
 
