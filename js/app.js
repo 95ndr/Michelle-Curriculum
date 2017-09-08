b2fbd373bc3c900b0619c006db2d2866404675a0
Michelle <an.darleen@outlook.com>
2017-09-07 22:15:12 -0600

    nuevos cambios

--- js/app.js
+++ js/app.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
 
-$('html').ready(function(){
-	 $(".f").slideUp(5000);
- });
-    
-    
-
 
  // canvas
-
-
 var dibujo= document.getElementById('cuadros');
 var lienzo = dibujo.getContext("2d");
 console.log(lienzo);
@@ -22,31 +14,12 @@ function dibujarY(){
   strokeStyle: 'white',
   strokeWidth: 1,
   x1: i, y1: 0,
-  x2: i, y2:799
+  x2: i, y2:699
 });
 	i+=10
 	}
 }
 dibujarY();
-
-/*	  	var $c = $("#cuadros");
-	var i = 5;
-	while(i < $c.height()){
-	$('canvas').drawLine({
-  strokeStyle: 'white',
-  strokeWidth: 1,
-  x1:0, y1: i,
-  x2:1399, y2:i
-});
-	i+=3
-	}
-}
-dibujarX();*/
-
-
-
-var $h = $("html");
-
 }); //fin de toda la app
 
 
