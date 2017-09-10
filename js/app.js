ccc18ecc13c89c3bfe7334513295b8f27324a465
Michelle <an.darleen@outlook.com>
2017-09-09 20:20:32 -0600

    arreglado primera fase de responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+responsive();
+dibujarY();
 // canvas
 function dibujarY(){
 	var $c = $("#cuadros");
@@ -20,17 +22,47 @@ function dibujarY(){
 });
 	i+=5
 	}
-}
-
-dibujarY();
+} //fin de funcion dibujar
 //responsive 
-
-$(window).resize(dibujarY);
-
-
-
-
-
+$(window).trigger('change', resize)
+$(window).resize(resize);
+function resize(){
+	responsive();
+	dibujarY();
+} // fin
+function responsive (){
+	var $deviceWidth = $(window).width();
+if($deviceWidth>320 && $deviceWidth<800){
+   $("div.paralelos").css({
+   	width: '90%',
+   	margin: '3%',
+   	minHeight:'100vh'
+   });
+   return;
+}else if($deviceWidth>800 && $deviceWidth<1024){
+	$("div.paralelos").css({
+   	width: '48.5%',
+   	margin:'0px',
+    height:'85vh'
+   });
+	return;
+}else if($deviceWidth>1024 && $deviceWidth<1200){
+	$("div.paralelos").css({
+   	width: '32.5%',
+   	marginLeft:'0.3%',
+    height:'85vh'
+   });
+}else if($deviceWidth>1200){
+	$("div.paralelos").css({
+   	  width: '28%',
+   	  minHeight:'90vh',
+      maxHeight: '90vh',
+      margin:'0px',
+      marginLeft:'3.5%'
+      
+   });
+}
+}//fin de la funcion responsive
 }); //fin de toda la app
 
 
